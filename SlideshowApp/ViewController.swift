4d72282c1d00d234235f3c11ae3e4c8e8805559f
tomohiro.ochi <t_ochi_1982@yahoo.co.jp>
2021-03-07 23:14:59 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Tomohiro Ochi on 2021/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
